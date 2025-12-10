ad90cf5308619dbb2d2268f9a9ee768ed52ea6e3
johnahlenhed <johnahlenhed@gmail.com>
2025-12-10 16:38:04 +0100

    Bugfix fetch

--- scripts/historic_mars_weather.js
+++ scripts/historic_mars_weather.js
@@ -1,4 +1,9 @@
-const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
+fetch("fetch-weather.php")
+  .then(res => res.json())
+  .then(data => console.log(data))
+  .catch(err => console.error(err));
+
+const API_URL = "fetch-weather.php";
 const historicWeatherElement = document.querySelector('[historic-mars-weather]');
 
 
