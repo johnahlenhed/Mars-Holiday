4fca453c2827197456673a8e87a2bef081b5e291
johnahlenhed <johnahlenhed@gmail.com>
2025-12-10 15:48:31 +0100

    Image sizing and enlarge on click

--- earth_images.php
+++ earth_images.php
@@ -1,8 +1,25 @@
 <?php include __DIR__ . '/includes/header.php'; ?>
 
 <main>
+        <div class="background-overlay"></div>
         <section class="earth-images"></section>
 </main>
+
+<script>
+
+        document.addEventListener('DOMContentLoaded', () => {
+            const earthImagesSection = document.querySelector('.earth-images');
+
+            earthImagesSection.addEventListener('click', (event) => {
+                if (event.target.tagName === 'IMG') {
+                    event.target.classList.toggle('enlarged');
+                    document.querySelector('.background-overlay').classList.toggle('active');
+                }
+
+            });
+        });
+
+</script>
 
 <?php include __DIR__ . '/includes/footer.php'; ?>
 
